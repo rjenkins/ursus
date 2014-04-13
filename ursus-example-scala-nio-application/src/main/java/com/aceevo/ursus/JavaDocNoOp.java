d36bee7b8421dbc507d36978abf8e935a60e9597
Ray Jenkins <rjenkins@aceevo.com>
2014-04-12 23:27:23 -0500

    adding java to scala projects to publish javadocs

---
+++ ursus-example-scala-nio-application/src/main/java/com/aceevo/ursus/JavaDocNoOp.java
@@ -0,0 +1,11 @@
+package com.aceevo.ursus;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rayjenkins
+ * Date: 4/12/14
+ * Time: 11:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavaDocNoOp {
+}
